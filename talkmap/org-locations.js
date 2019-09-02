9e7db2c9fc46096cc61707ee5449d21574132eec
tombewley <bewster1@gmail.com>
2019-09-02 15:03:33 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,6 +1,6 @@
 var addressPoints = [
   [
-    "Bristol, UK<br/><a href=\"https://google.com\">TEST</a>"
+    "Bristol, UK<br/><a href=\"https://google.com\">TEST</a>",
     51.4538022,
     -2.5972985
   ]
