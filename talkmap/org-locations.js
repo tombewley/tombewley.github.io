49c7a16fa62816652775f02e8fe3023f4cdf9926
tombewley <bewster1@gmail.com>
2019-09-02 15:57:36 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,5 +1,10 @@
 var addressPoints = [
   [
+    "Bristol, UK<br/><a href='https://tombewley.com/talks/2019-09-02-test' target='_top'>Test Talk to Delete</a>",
+    51.4538022,
+    -2.5972985
+  ],
+  [
     "Bristol, UK<br/><a href='https://tombewley.com/talks/2019-10-03-explainability-pecha-kucha' target='_top'>PechaKucha: Explainable AI and Social Responsibility</a>",
     51.4538022,
     -2.5972985
