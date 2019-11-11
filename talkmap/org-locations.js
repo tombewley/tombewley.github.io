9170cfbe48105b85b8eba1f53d307b2381bac095
tombewley <bewster1@gmail.com>
2019-11-11 11:19:02 +0000

    Add DSRS-Turing talk

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,12 +1,12 @@
 var addressPoints = [
   [
-    "Bristol, UK<br/><a href='https://tombewley.com/talks/2019-09-02-test' target='_top'>Test Talk to Delete</a><br/>September 02, 2019",
+    "Bristol, UK<br/><a href='https://tombewley.com/talks/2019-10-03-explainability-pecha-kucha' target='_top'>Scary Black Boxes: Why Explanation Lies at the Heart of Socially-responsible AI</a><br/>October 03, 2019",
     51.4538022,
     -2.5972985
   ],
   [
-    "Bristol, UK<br/><a href='https://tombewley.com/talks/2019-10-03-explainability-pecha-kucha' target='_top'>PechaKucha: Explainable AI and Social Responsibility</a><br/>October 03, 2019",
-    51.4538022,
-    -2.5972985
+    "London, UK<br/><a href='https://tombewley.com/talks/2019-11-03-ontour-dsrs-turing' target='_top'>On Tour: Harnessing Social Tourism Data for City and Point of Interest Recommendation</a><br/>November 21, 2019",
+    51.5073219,
+    -0.1276474
   ]
 ];
